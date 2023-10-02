aeb32acbe1af7c34a9000d328f6b19502e280a8c
Sarfaroj Ansari <sarfarosha66@gmail.com>
2023-10-02 16:12:14 +0530

    add logic to place marker

--- index.js
+++ index.js
@@ -3,35 +3,32 @@ document.addEventListener('DOMContentLoaded', () => {
     const GameBoard = (() => {
         let rows = 3;
         let columns = 3;
-        let board = [
-            ["X", "X", "X"],
-            ["O", "O", "O"],
-            ["X", "X", "X"]
-        ];
+        let board = [];
     
-        // for (let i = 0; i < rows; i++) {
-        //     board[i] = []
-        //     for (let j = 0; j < columns; j++) {
-        //         board[i].push(0);
-        //     }
-        // } 
+        for (let i = 0; i < rows; i++) {
+            board[i] = []
+            for (let j = 0; j < columns; j++) {
+                board[i].push("");
+            }
+        } 
         return {board}
     }) ();
     
     
     // factory function
-    const Player = (name, marker, move) => {
+    const Player = (name, marker) => {
         const sayHello = () => console.log(`Hello! my name is ${name}`);
-        return { name, marker, move, sayHello };
+        return { name, marker, sayHello };
     };
     
     
-    const position = (row, column) => {
-        GameBoard.board[row][column] = 1;   
+    const addMarker = (row, column, marker, board) => {
+        board[row][column] = marker;   
         // TODO
     }
+
     
-    const displayController = (() => {
+    const displayController = () => {
         // take array and show it on the display
         const gameArea = document.getElementById("gameArea");
         const gameCells = document.querySelectorAll('div.gameCell');
@@ -43,11 +40,56 @@ document.addEventListener('DOMContentLoaded', () => {
                 const divIndex = rowIndex * 3 + colIndex;
 
                 // set the value form the array to the div element
-
                 gameCells[divIndex].textContent = element;
             })
         })
-    }) ();
+    };
+
+
+    // add markers to a specific spot 
+    const game = () => {
+        // have a board set up
+        const board = GameBoard.board;
+
+        const gameCells = document.querySelectorAll('.gameCell');
+
+        const p1 = Player("s", "X");
+        const p2 = Player('r', "O");
+
+        // move count
+        let movesCount = 0;
+        let turnInfo = document.getElementById('turnInfo');
+        turnInfo.textContent = "Turn: Player 1"
+
+        gameCells.forEach((cell) => {
+            cell.addEventListener('click', () => {
+                
+                let row = cell.dataset.row;
+                let column = cell.dataset.column;
+
+                // if it is a valid move then increment moveCount
+
+                if (!board[row][column]) {
+                    movesCount++;
+                    turnInfo.textContent = "Turn: Player 1"
+                    if (movesCount % 2 != 0) {
+                        turnInfo.textContent = "Turn: Player 2"
+                        board[row][column] = p1.marker;
+                        displayController();
+                    } else {
+                        turnInfo.textContent = "Turn: Player 1"
+                        board[row][column] = p2.marker;
+                        displayController();
+                    }
+
+                }
+                
+            })
+        })
+    }
+
+    game();
+    displayController();
 })
 
 
